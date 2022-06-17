4c42f7fd70513aeb6c2a5355cdc7d80681ee6ec3
Fox Flaherty <katyra14@gmail.com>
2022-06-16 23:51:41 -0700

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-require('dotenv').config()
+require('dotenv').config() //must always be at top
 const express = require('express')
 const app = express()
 
 app.get('/', function(req, res) {
-    res.send("Hello World!")
+    res.send("Hello World!") // this must be start for homepage any others must come after
 })
+
+app.get('*',  (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+// ??  (req, res) => another way to make a function??
 
 app.listen(process.env.PORT, function(req, res) {
     console.log('Connected to Foxnet')
-})
+}) // must always be at bottom
